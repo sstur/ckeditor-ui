8f98f9f83f32913a9e0a4b13716544bf1ce765a4
Simon Sturmer <sstur@me.com>
2012-09-04 12:18:21 +0700

    remove .idea; style richcombo

--- config.js
+++ config.js
@@ -5,6 +5,6 @@
 
 CKEDITOR.editorConfig = function( config ) {
 	// Just put the name of your custom skin here.
-	config.skin = 'ozone';
+	config.skin = location.search.match(/[&?]old([&=]|$)/) ? 'kama' : 'ozone';
 };
 
